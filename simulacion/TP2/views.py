e8fa13e16dfab2f2e5876590b96b8e2255ca0d40
Tomas Sator <tomasmsator@gmail.com>
2022-04-06 20:49:46 -0300

    TP2 - histrograma muestra1

--- simulacion/TP2/views.py
+++ simulacion/TP2/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from TP2.funcionesExcel import datosNbaEspn
+from TP2.funcionesExcel import datosNbaEspn, histograma
 # Create your views here.
 
+datos_histograma = histograma.Histograma()
 
 def muestra1(request):
-
     datos_muestra = datosNbaEspn.stats_ginobili()
-    return render(request, 'muestra1.html', {'datos_muestra':datos_muestra})
+
+    datos_histograma.determinarIntervalo(15, datos_muestra, len(datos_muestra))
+
+    return render(request, 'muestra1.html', {'datos_muestra':datos_muestra, 'datos_histograma':datos_histograma.intervalos})
